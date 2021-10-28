498a1d6271aab3a0782b8f9425dc8e5c6fe00936
samsontesta <niewiemstudio@gmail.com>
2021-10-28 17:54:59 +0200

    Initial commit

---
+++ src/test/java/lukaszsamson/StudentList/StudentListApplicationTests.java
@@ -0,0 +1,13 @@
+package lukaszsamson.StudentList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
