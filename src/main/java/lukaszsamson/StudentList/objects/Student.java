8e03ecfc3f2e463c648ce6fdef17066bf348cb91
samsontesta <niewiemstudio@gmail.com>
2021-10-28 20:04:02 +0200

    pom.xml add swagger dependensis, create Student.java class

---
+++ src/main/java/lukaszsamson/StudentList/objects/Student.java
@@ -0,0 +1,53 @@
+package lukaszsamson.StudentList.objects;
+
+import javax.swing.*;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String surname;
+    private int phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
