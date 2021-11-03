7c4b4d8df31217d23feb3cb549893436a0308fe7
samsontesta <niewiemstudio@gmail.com>
2021-11-03 21:04:56 +0100

    pom.xml add swagger dependensis, create Student.java class

--- src/main/java/lukaszsamson/StudentList/StudentListApplication.java
+++ src/main/java/lukaszsamson/StudentList/StudentListApplication.java
@@ -2,8 +2,10 @@ package lukaszsamson.StudentList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class StudentListApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/lukaszsamson/StudentList/aps/StudentApi.java
@@ -0,0 +1,30 @@
+package lukaszsamson.StudentList.aps;
+
+import lukaszsamson.StudentList.objects.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class StudentApi {
+    private List<Student> studentList;
+    public StudentApi(){
+        this.studentList = new ArrayList<>();
+    }
+    @PostMapping("/addStudent")
+    public boolean addStudent(@RequestBody Student student){
+        return studentList.add(student);
+    }
+    @GetMapping("/getList")
+    public List<Student> getStudentList(){
+        return studentList;
+
+    }
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentList.stream().filter(student -> student.getId() == id).findFirst().get();
+
+    }
+}
